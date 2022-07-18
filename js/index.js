187909b6f3b2acc9026dd6e28e5feb1401a69c3f
Nash Zangio <nash.c.zangio@gmail.com>
2022-07-18 18:39:49 +0200

    first iteration

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
 // Iteration 1: Names and Input
-//
+
+let hacker1 = "Nash"
+console.log(`The driver's name is ${hacker1}`)
+
+let hacker2 = "Lloyd"
+console.log(`The navigator's name is ${hacker2}`)
+
+
+
 // Iteration 2: Conditionals
 
 
+
 // Iteration 3: Loops
+
+
+
+
+
+
+
+
